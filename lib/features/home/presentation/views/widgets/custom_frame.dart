7e391843ed93489b26d8986c8b86c8cd31e746fe
Malek <malekyasser10@gmail.com>
2024-09-06 18:47:04 +0300

    height of frame is now randomly generated to create that staggered feeling

--- lib/features/home/presentation/views/widgets/custom_frame.dart
+++ lib/features/home/presentation/views/widgets/custom_frame.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,6 +20,7 @@ class CustomFrame extends StatefulWidget {
 }
 
 class _CustomFrameState extends State<CustomFrame> {
+  final Map<int, double> _imageHeights = {};
   @override
   Widget build(BuildContext context) {
     bool isNetworkImage = widget.product.image.startsWith('http');
@@ -39,136 +42,142 @@ class _CustomFrameState extends State<CustomFrame> {
         } else if (state is RemoveLikeSuccessState && state.productID == widget.product.id) {
           currentLikeCounter = state.likeCounter;
         }
-
+        if (!_imageHeights.containsKey(widget.product.id)) {
+          _imageHeights[widget.product.id] = (150.0 + Random().nextInt(51)).clamp(150, 200) as double;
+        }
+        double imageHeight = _imageHeights[widget.product.id]!;
         bool isLiked = lCubit.likedProducts.containsKey(widget.product.id);
 
-        return Container(
-          decoration: BoxDecoration(
-            color: AppColors.offWhite,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          DescriptionView(productId: widget.product.id),
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: const Color(0xFFE3DDDD),
-                        width: 2,
+        return Padding(
+          padding:  EdgeInsets.only(top :imageHeight/28),
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColors.offWhite,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DescriptionView(productId: widget.product.id),
                       ),
-                    ),
-                    child: ClipRRect(
-                      clipBehavior: Clip.antiAlias,
-                      borderRadius: BorderRadius.circular(10),
-                      child: isNetworkImage
-                          ? CachedNetworkImage(
-                        imageUrl: widget.product.image,
-                        fit: BoxFit.cover,
-                        height: 150,
-                        width: double.infinity,
-                        placeholder: (context, url) => Shimmer.fromColors(
-                          baseColor: AppColors.lighterGreen,
-                          highlightColor: AppColors.barelyGreen,
-                          child: Container(
-                            color: Colors.white,
-                            height: 150,
-                            width: double.infinity,
-                          ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: const Color(0xFFE3DDDD),
+                          width: 2,
                         ),
-                        errorWidget: (context, url, error) {
-                          return Image.asset(
-                            'assets/images/plant6.png',
-                            fit: BoxFit.cover,
-                            height: 150,
-                            width: double.infinity,
-                          );
-                        },
-                        fadeInDuration: const Duration(milliseconds: 500),
-                        fadeOutDuration: const Duration(milliseconds: 500),
-                      )
-                          : Image.asset(
-                        widget.product.image,
-                        fit: BoxFit.cover,
-                        height: 150,
-                        width: double.infinity,
+                      ),
+                      child: ClipRRect(
+                        clipBehavior: Clip.antiAlias,
+                        borderRadius: BorderRadius.circular(10),
+                        child: isNetworkImage
+                            ? CachedNetworkImage(
+                          imageUrl: widget.product.image,
+                          fit: BoxFit.cover,
+                          height: imageHeight,
+                          width: double.infinity,
+                          placeholder: (context, url) => Shimmer.fromColors(
+                            baseColor: AppColors.lighterGreen,
+                            highlightColor: AppColors.barelyGreen,
+                            child: Container(
+                              color: Colors.white,
+                              height: 150,
+                              width: double.infinity,
+                            ),
+                          ),
+                          errorWidget: (context, url, error) {
+                            return Image.asset(
+                              'assets/images/plant6.png',
+                              fit: BoxFit.cover,
+                              height: 150,
+                              width: double.infinity,
+                            );
+                          },
+                          fadeInDuration: const Duration(milliseconds: 500),
+                          fadeOutDuration: const Duration(milliseconds: 500),
+                        )
+                            : Image.asset(
+                          widget.product.image,
+                          fit: BoxFit.cover,
+                          height: 150,
+                          width: double.infinity,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, left: 8, right: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        widget.product.productName,
-                        style: TextStyle(
-                          fontFamily: "Poppins",
-                          fontWeight: FontWeight.bold,
-                          fontSize: getResponsiveSize(context, fontSize: 15),
+                const SizedBox(height: 5),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 8, right: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          widget.product.productName,
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.bold,
+                            fontSize: getResponsiveSize(context, fontSize: 15),
+                          ),
                         ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        if (!isLiked) {
-                          lCubit.addLikedProducts(widget.product.id);
-                        } else {
-                          lCubit.removeLikedProducts(widget.product.id);
-                        }
-                      },
-                      icon: Icon(
-                        isLiked ? Icons.favorite : Icons.favorite_border,
-                        color: isLiked ? Colors.red : Colors.black,
-                        size: getResponsiveSize(context, fontSize: 20),
+                      IconButton(
+                        onPressed: () {
+                          if (!isLiked) {
+                            lCubit.addLikedProducts(widget.product.id);
+                          } else {
+                            lCubit.removeLikedProducts(widget.product.id);
+                          }
+                        },
+                        icon: Icon(
+                          isLiked ? Icons.favorite : Icons.favorite_border,
+                          color: isLiked ? Colors.red : Colors.black,
+                          size: getResponsiveSize(context, fontSize: 20),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0, left: 8, right: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        "${widget.product.price} EGP",
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0, left: 8, right: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          "${widget.product.price} EGP",
+                          style: TextStyle(
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.bold,
+                            fontSize: getResponsiveSize(context, fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "$currentLikeCounter likes",
                         style: TextStyle(
                           fontFamily: "Raleway",
                           fontWeight: FontWeight.bold,
                           fontSize: getResponsiveSize(context, fontSize: 15),
                         ),
                       ),
-                    ),
-                    Text(
-                      "$currentLikeCounter likes",
-                      style: TextStyle(
-                        fontFamily: "Raleway",
-                        fontWeight: FontWeight.bold,
-                        fontSize: getResponsiveSize(context, fontSize: 15),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
